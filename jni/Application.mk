4c218c28c7cf999f8ca60b9f91d196719124ec5c
ksmith <ksmith@ndlocis.mil.no>
2021-01-14 16:38:41 +0100

    armeabi is not supported anymore, and platform must be set. Set platform to all, to compile for all supported platforms.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,6 @@
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := all
+#APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
+APP_PLATFORM := android-19
 APP_MODULES := lwip-android lwip-android-jni
 APP_OPTIM := release
